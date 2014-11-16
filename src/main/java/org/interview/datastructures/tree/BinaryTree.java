527665e413fb50479d8000ab0ed731df2b01d179
user-a-github <adobrov@gmail.com>
2014-11-15 22:50:29 -0500

    Binary Tree breadth first traversal

--- src/main/java/org/interview/datastructures/tree/BinaryTree.java
+++ src/main/java/org/interview/datastructures/tree/BinaryTree.java
@@ -1,8 +1,7 @@
 package org.interview.datastructures.tree;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.interview.utils.Math.ge;
 
@@ -11,6 +10,10 @@ public class BinaryTree<T extends Comparable<T>> {
         public T value;
         public Node<T> left;
         public Node<T> right;
+
+        public Node(T value) {
+            this.value = value;
+        }
     }
 
     private Node<T> root;
@@ -18,30 +21,45 @@ public class BinaryTree<T extends Comparable<T>> {
     public void add(T item) {
         if (item == null) throw new IllegalArgumentException("Can't add nulls to the tree.");
         if (root == null) {
-            root = new Node<T>();
-            root.value = item;
+            root = new Node<T>(item);
         } else {
+            addToTree(item);
+        }
+    }
+
+    public List<T> breadthFirst() {
+        List<T> nodeValues = new ArrayList<T>();
+        if (root != null) {
+            Queue<Node<T>> queue = new LinkedList<Node<T>>();
+            queue.add(root);
+            Node<T> it;
+            while (!queue.isEmpty()) {
+                it = queue.remove();
+                nodeValues.add(it.value);
+                if (it.left != null) queue.add(it.left);
+                if (it.right != null) queue.add(it.right);
+            }
+        }
+        return nodeValues;
+    }
+
+    public List<T> inOrderWithIteration() {
+        List<T> nodeValues = new ArrayList<T>();
+        if (root != null) {
+            Stack<Node<T>> stack = new Stack<Node<T>>();
             Node<T> it = root;
-            while (it != null) {
-                if (ge(item, it.value)) {
-                    if (it.right != null) {
-                        it = it.right;
-                    } else {
-                        it.right = new Node<T>();
-                        it.right.value = item;
-                        it = null;
-                    }
+            while (!stack.isEmpty() || it != null) {
+                if (it != null) {
+                    stack.add(it);
+                    it = it.left;
                 } else {
-                    if (it.left != null) {
-                        it = it.left;
-                    } else {
-                        it.left = new Node<T>();
-                        it.left.value = item;
-                        it = null;
-                    }
+                    it = stack.pop();
+                    nodeValues.add(it.value);
+                    it = it.right;
                 }
             }
         }
+        return nodeValues;
     }
 
     public List<T> inOrder() {
@@ -83,6 +101,27 @@ public class BinaryTree<T extends Comparable<T>> {
             postOrder(node.left, values);
             postOrder(node.right, values);
             values.add(node.value);
+        }
+    }
+
+    private void addToTree(T item) {
+        Node<T> it = root;
+        while (it != null) {
+            if (ge(item, it.value)) {
+                if (it.right != null) {
+                    it = it.right;
+                } else {
+                    it.right = new Node<T>(item);
+                    it = null;
+                }
+            } else {
+                if (it.left != null) {
+                    it = it.left;
+                } else {
+                    it.left = new Node<T>(item);
+                    it = null;
+                }
+            }
         }
     }
 }
--- src/test/java/org/interview/datastructures/tree/BinaryTreeTest.java
+++ src/test/java/org/interview/datastructures/tree/BinaryTreeTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class BinaryTreeTest {
-    private static final Integer[] INPUT_VALUES = {100, -8, 105, 306, 0, 0, 0, 50, -5, 55, 60, 150, 102, 10, 3};
     /*
                                         100
                        -8                                   105
@@ -20,6 +19,8 @@ public class BinaryTreeTest {
                                      3                   60
      */
     private BinaryTree<Integer> tree;
+    private static final Integer[] INPUT_VALUES = {100, -8, 105, 306, 0, 0, 0, 50, -5, 55, 60, 150, 102, 10, 3};
+    private static final Integer[] BREADTH_FIRST_EXPECTED = new Integer[]{100, -8, 105, 0, 102, 306, -5, 0, 150, 0, 50, 10, 55, 3, 60};
     private static final Integer[] IN_ORDER_EXPECTED = new Integer[]{-8, -5, 0, 0, 0, 3, 10, 50, 55, 60, 100, 102, 105, 150, 306};
     private static final Integer[] PRE_ORDER_EXPECTED = new Integer[]{100, -8, 0, -5, 0, 0, 50, 10, 3, 55, 60, 105, 102, 306, 150};
     private static final Integer[] POST_ORDER_EXPECTED = new Integer[]{-5, 3, 10, 60, 55, 50, 0, 0, 0, -8, 102, 150, 306, 105, 100};
@@ -38,6 +39,25 @@ public class BinaryTreeTest {
     public void shouldAddNotNullElements() {
         addValueToTree(INPUT_VALUES);
         assertTree(tree, IN_ORDER_EXPECTED, PRE_ORDER_EXPECTED, POST_ORDER_EXPECTED);
+    }
+
+    @Test
+    public void shouldTraversBreadthFirstForEmptyTree() {
+        assertTrue(tree.breadthFirst().isEmpty());
+    }
+
+    @Test
+    public void shouldTraversBreadthFirstForOneElementAndTwoElementsTree() {
+        tree.add(10);
+        assertThat(tree.breadthFirst(), contains(10));
+        tree.add(0);
+        assertThat(tree.breadthFirst(), contains(10, 0));
+    }
+
+    @Test
+    public void shouldTraversBreadthFirst() {
+        addValueToTree(INPUT_VALUES);
+        assertThat(tree.breadthFirst(), contains(BREADTH_FIRST_EXPECTED));
     }
 
     @Test
@@ -81,20 +101,24 @@ public class BinaryTreeTest {
     @Test
     public void shouldTraversInOrderForEmptyTree() {
         assertTrue(tree.inOrder().isEmpty());
+        assertTrue(tree.inOrderWithIteration().isEmpty());
     }
 
     @Test
     public void shouldTraversInOrderForOneElementAndTwoElementsTree() {
         tree.add(10);
         assertThat(tree.inOrder(), contains(10));
+        assertThat(tree.inOrderWithIteration(), contains(10));
         tree.add(0);
         assertThat(tree.inOrder(), contains(0, 10));
+        assertThat(tree.inOrderWithIteration(), contains(0, 10));
     }
 
     @Test
     public void shouldTraversInOrder() {
         addValueToTree(INPUT_VALUES);
         assertThat(tree.inOrder(), contains(IN_ORDER_EXPECTED));
+        assertThat(tree.inOrderWithIteration(), contains(IN_ORDER_EXPECTED));
     }
 
     private void assertTree(BinaryTree<Integer> tree, Integer[] inOrder, Integer[] preOrder, Integer[] postOrder) {
