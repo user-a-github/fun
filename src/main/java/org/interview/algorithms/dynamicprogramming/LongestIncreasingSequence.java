e57b56b92e9523f0745f51f77150228d32f409f9
user-a-github <adobrov@gmail.com>
2014-11-28 22:39:02 -0500

    Graph DFS traversal (refactored)

--- src/main/java/org/interview/algorithms/dynamicprogramming/LongestIncreasingSequence.java
+++ src/main/java/org/interview/algorithms/dynamicprogramming/LongestIncreasingSequence.java
@@ -4,44 +4,47 @@ import static java.lang.System.arraycopy;
 
 public class LongestIncreasingSequence {
 
+    private static class Item {
+        public final int value;
+        public final int subSequenceLength;
+
+        public Item(int value, int subSequenceLength) {
+            this.value = value;
+            this.subSequenceLength = subSequenceLength;
+        }
+    }
+
     public static int[] lis(int[] sequence) {
         if (sequence == null) throw new IllegalArgumentException("Sequence can't be null");
         if (sequence.length == 0) {
             return new int[0];
         } else {
-            int[][] function = new int[2][sequence.length];
-            int[][] sequenceLength = new int[2][sequence.length];
+            Item[][] function = new Item[2][sequence.length];
 
             int i = sequence.length - 1;
-            function[0][i] = sequence[i];
-            function[1][i] = 0;
-            sequenceLength[0][i] = 1;
-            sequenceLength[1][i] = 0;
+            function[0][i] = new Item(sequence[i], 1);
+            function[1][i] = new Item(0, 0);
             int iPos = 0;
             while (--i >= 0) {
-                if (0 > function[0][i + 1]) {
-                    function[0][i] = sequence[i];
-                    sequenceLength[0][i] = 1;
+                if (0 > function[0][i + 1].value) {
+                    function[0][i] = new Item(sequence[i], 1);
                 } else {
-                    function[0][i] = sequence[i] + function[0][i + 1];
-                    sequenceLength[0][i] = sequenceLength[0][i + 1] + 1;
+                    function[0][i] = new Item(sequence[i] + function[0][i + 1].value, function[0][i + 1].subSequenceLength + 1);
                 }
-                if (function[0][i + 1] > function[1][i + 1]) {
+                if (function[0][i + 1].value > function[1][i + 1].value) {
                     function[1][i] = function[0][i + 1];
-                    sequenceLength[1][i] = sequenceLength[0][i + 1];
                     iPos = i + 1;
                 } else {
                     function[1][i] = function[1][i + 1];
-                    sequenceLength[1][i] = sequenceLength[1][i + 1];
                 }
             }
 
             int[] subSequence;
-            if (function[0][0] >= function[1][0]) {
-                subSequence = new int[sequenceLength[0][0]];
+            if (function[0][0].value >= function[1][0].value) {
+                subSequence = new int[function[0][0].subSequenceLength];
                 arraycopy(sequence, 0, subSequence, 0, subSequence.length);
             } else {
-                subSequence = new int[sequenceLength[1][0]];
+                subSequence = new int[function[1][0].subSequenceLength];
                 arraycopy(sequence, iPos, subSequence, 0, subSequence.length);
             }
             return subSequence;
