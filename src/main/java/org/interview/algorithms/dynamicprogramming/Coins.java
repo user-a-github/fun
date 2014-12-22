9396a308e9ee739ae06170c49b18ffcda182418b
user-a-github <adobrov@gmail.com>
2014-12-21 20:28:20 -0500

    DP minimal coins number to return change

--- src/main/java/org/interview/algorithms/dynamicprogramming/Coins.java
+++ src/main/java/org/interview/algorithms/dynamicprogramming/Coins.java
@@ -1,5 +1,7 @@
 package org.interview.algorithms.dynamicprogramming;
 
+import static java.lang.Integer.MAX_VALUE;
+
 public class Coins {
     public static int[] COINS = {1, 3, 5, 10};
 
@@ -16,6 +18,21 @@ public class Coins {
             function[i] = n;
         }
 
+        return function[change];
+    }
+
+    public static int minimumCoinsToReturnChange(int change) {
+        if (change < 0) throw new IllegalArgumentException("Change can't be negative.");
+        int[] function = new int[change + 1];
+        function[0] = 0;
+        for (int i = 1; i < function.length; i++) {
+            int min = MAX_VALUE;
+            for (int j = 0; j < COINS.length; j++) {
+                if (i >= COINS[j] && min > function[i - COINS[j]])
+                    min = function[i - COINS[j]];
+            }
+            function[i] = min + 1;
+        }
         return function[change];
     }
 }
--- src/test/java/org/interview/algorithms/dynamicprogramming/CoinsTest.java
+++ src/test/java/org/interview/algorithms/dynamicprogramming/CoinsTest.java
@@ -3,6 +3,7 @@ package org.interview.algorithms.dynamicprogramming;
 import org.junit.Test;
 
 import static org.interview.algorithms.dynamicprogramming.Coins.allCombinationsToReturnChange;
+import static org.interview.algorithms.dynamicprogramming.Coins.minimumCoinsToReturnChange;
 import static org.junit.Assert.assertEquals;
 
 public class CoinsTest {
@@ -10,6 +11,7 @@ public class CoinsTest {
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowExceptionForNegativeChange() {
         allCombinationsToReturnChange(-1);
+        minimumCoinsToReturnChange(-1);
     }
 
     @Test
@@ -23,8 +25,24 @@ public class CoinsTest {
     }
 
     @Test
-    public void shouldReturnNumberForChangeGrateMaximumCoin() {
+    public void shouldReturnNumberForChangeMaximumCoin() {
         assertEquals(76, allCombinationsToReturnChange(11));
+    }
+
+    @Test
+    public void shouldReturnMinimumNumberForZeroChange() {
+        assertEquals(0, minimumCoinsToReturnChange(0));
+    }
+
+    @Test
+    public void shouldReturnMinimumNumberForChangeLessThenMaximumCoin() {
+        assertEquals(3, minimumCoinsToReturnChange(7));
+        assertEquals(2, minimumCoinsToReturnChange(8));
+    }
+
+    @Test
+    public void shouldReturnMinimumNumberForChange() {
+        assertEquals(2, minimumCoinsToReturnChange(11));
     }
 
 }
